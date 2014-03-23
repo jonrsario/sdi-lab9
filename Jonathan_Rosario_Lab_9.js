20b4d8a1a1cb4d84d96c066208b2759c4ff7a92f
Jonathan Rosario <archidynamics@yahoo.com>
2014-03-22 22:04:13 -0400

    rev 3

--- Jonathan_Rosario_Lab_9.js
+++ Jonathan_Rosario_Lab_9.js
@@ -16,7 +16,7 @@ var myDec = function(myNum1, myNum2) {
 		
 };
 
-console.log("The number " + myNum1 + " is now a decimal " + myDec(myNum1, myNum2));
+console.log("The number " + myNum1 + " is now called Mr. Decimal " + myDec(myNum1, myNum2));
 
 
 //Problem 2
@@ -35,28 +35,12 @@ console.log("Hi, I'm " + numString(numAsString) + " and I am a recovering string
 
 //Problem 3
 
-var myFirstDate = "3/12/2014";
-var mySecondDate = "3/22/2014";
-var time = prompt("Enter the number of days or hours");
+var today = new Date();
+var nextWeek = new Date("March 29, 2014");
 
-var dateType = function(myFirstDate, mySecondDate, time) {
-	
-	var a = new Date(myFirstDate);
-	var b = new Date(mySecondDate);
-	var myChangedDates = mySecondDate - myFirstDate;
-	
-	console.log(myChangedDates);
-	
-	if (time === "hours") {
-	
-	return ((myChangedDates/1000)/60/60);
-	
-	}else if (time === "days") {
-		
-		return ((myChangedDates/1000/60/60)/24);
-		
-	}	
+console.log(today)
+console.log(nextWeek);
 
-};
+var inBetween = nextWeek - today;
 
-console.log("The dates have been successfully changed " + dateType(myFirstDate, myFirstDate, time));
+console.log("Jessica Alba will be my wife in " + Math.ceil(inBetween/1000/60/60/24) + " days.");
