e4fe3f7a7e747ea52ed0c1a28c1c7c7056a1ea50
Jonathan Rosario <archidynamics@yahoo.com>
2014-03-22 21:44:45 -0400

    rev2

--- Jonathan_Rosario_Lab_9.js
+++ Jonathan_Rosario_Lab_9.js
@@ -8,7 +8,7 @@
 var myNum1 = 5;
 var myNum2 = 2;
 
-var myDec = function(num1, num2) {
+var myDec = function(myNum1, myNum2) {
 	
 	var newNum = myNum1.toFixed(myNum2);
 	
@@ -16,7 +16,7 @@ var myDec = function(num1, num2) {
 		
 };
 
-console.log("The number is now a decimal " + myDec(myNum1, myNum2));
+console.log("The number " + myNum1 + " is now a decimal " + myDec(myNum1, myNum2));
 
 
 //Problem 2
@@ -31,32 +31,32 @@ var numString = function(numAsString) {
 	
 };
 
-console.log("Hi, I'm " + numString(numAsString) + " and I am a string addict.");
+console.log("Hi, I'm " + numString(numAsString) + " and I am a recovering string addict.");
 
 //Problem 3
 
 var myFirstDate = "3/12/2014";
 var mySecondDate = "3/22/2014";
-var days = prompt("Enter days");
+var time = prompt("Enter the number of days or hours");
 
-var dateType = function(myFirstDate, mySecondDate, days) {
+var dateType = function(myFirstDate, mySecondDate, time) {
 	
 	var a = new Date(myFirstDate);
 	var b = new Date(mySecondDate);
-	var variationBetweenDates = a - b;
+	var myChangedDates = mySecondDate - myFirstDate;
 	
-	console.log(variationBetweenDates);
+	console.log(myChangedDates);
 	
-	if (days === "hours") {
+	if (time === "hours") {
 	
-	return ((variationBetweenDates/1000)/60/60);
+	return ((myChangedDates/1000)/60/60);
 	
-	}else if (days === "days") {
+	}else if (time === "days") {
 		
-		return ((variationBetweenDates/1000/60/60)/24);
+		return ((myChangedDates/1000/60/60)/24);
 		
 	}	
 
 };
 
-console.log("The dates have been successfully changed " + dateType(myFirstDate, myFirstDate, days));
+console.log("The dates have been successfully changed " + dateType(myFirstDate, myFirstDate, time));
